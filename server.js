57dfff737ac9c6e6af99e06fd9be9dc39b912bee
AmirGillani <83864322+AmirGillani@users.noreply.github.com>
2024-07-18 10:15:51 -0700

    Update server.js

--- server.js
+++ server.js
@@ -28,19 +28,6 @@ const cookieParser = require("cookie-parser");
 
 const app = express();
 
-// INCLUDE THESE OPTIONS IF WE WANT TO SEND COOKIES TO FRONTEND
-
-const corsOptions = {
-
-  // SET ORIGION
-  origin: 'https://ecommerce-frontend-pi-one.vercel.app',
-  credentials: true, // Allows cookies to be sent cross-origin
-};
-
-// CALL API TO HANDLE RESPONCE GOING OUT TO REACT
-
-app.use(cors(corsOptions));
-
 // FOR JASON REQUESTS
 
 app.use(express.json());
